65da765f7c9e70025aa8b9dd809e6045ba9569aa
Orkun Karaduman <orkunkaraduman@gmail.com>
2022-11-06 02:44:38 +0300

    updated example1

--- examples/example1/main.go
+++ examples/example1/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"os"
+	"time"
+
+	"github.com/goinsane/filelock"
+)
+
 func main() {
-	//var f *filelock.File
+	f, err := filelock.Obtain(os.TempDir() + string(os.PathSeparator) + "filelock.test")
+	if err != nil {
+		panic(err)
+	}
+	//goland:noinspection GoUnhandledErrorResult
+	defer f.Release()
+	time.Sleep(10 * time.Second)
 }
